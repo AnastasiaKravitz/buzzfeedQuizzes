1506e569c1994a0de1c1ef8f4c27160ed5b8adf3
AnastasiaKravitz <an.kravitz7@gmail.com>
2024-06-05 21:16:06 +0300

    blahhh

---
+++ home/home.js
@@ -0,0 +1,15 @@
+function timeEraQuizz(){
+    window.location.href = "../Anastasia/timeEraQuizz/timeEraQuizz.html";
+}
+
+function genreQuizz(){
+    window.location.href = "../Anastasia/genreQuizz/genreQuizz.html";
+}
+
+function aestheticQuizz(){
+    window.location.href = "../Sofia/aestheticQuizz/aestheticQuizz.html";
+}
+
+function styleQuizz(){
+    window.location.href = "../Sofia/styleQuizz/styleQuizz.html";
+}
